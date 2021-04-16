9172a64bb7fc38cd70b1f02b38775545dc033c76
privatebrycehao <brycehao@gmail.com>
2021-04-15 22:35:14 -0700

    Update closure.js

--- Javascript basic questions/closure.js
+++ Javascript basic questions/closure.js
@@ -1,20 +1,9 @@
-let add = function(){
-      let counter = 0;
+var add = (function(){
+      var counter = 0;
       return function(){
              return(++counter);
       }
-};
-add()();   //counter为1
-add()();   //counter为1
-add()();   //counter为1
-
-
-let add = (function(){
-      let counter = 0;
-      return function(){
-             return(++counter);
-      }
-})();
+})();      //这里add已经是执行过后的函数了。即add指定了函数自我调用的返回值
 add();   //counter为1
-add();   //counter为1
-add();   //counter为1
+add();   //counter为2
+add();   //counter为3
